c2ce5755bad197c14716468d0e6dc3974a05c709
Roy Triesscheijn <roy-t@hotmail.com>
2017-07-27 19:34:01 +0200

    Update ignore file, remove benchmark results from repo

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -62,29 +62,7 @@ namespace Benchmark
             //{
             //    bench.TestLargeEmptyGrid();
             //}
-            //BenchmarkRunner.Run<AStarBenchmark>();
-
-            int[] num = { 69, 56, 77, 44, 8, 0, 111, 566 };
-            for (int i = 1; i < num.Length; i++)
-            {
-                int key = num[i];
-                int j = i - 1;
-                while (j >= 0 && num[j] > key)
-                {
-                    //loop
-                    num[j + 1] = num[j];
-                    j--;
-                }
-                num[j + 1] = key;
-            }
-
-            //display
-            foreach (var item in num)
-            {
-                Console.WriteLine(item);
-            }
-
-
+            BenchmarkRunner.Run<AStarBenchmark>();            
             Console.ReadLine();
         }
     }
