08bf23c3881978c6fccf6613fcdf4ceb8a34f2be
Roy Triesscheijn <roy-t@hotmail.com>
2024-04-03 10:35:34 +0200

    Add test case

--- Roy-T.AStar.Tests/PathFinderTests.cs
+++ Roy-T.AStar.Tests/PathFinderTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using Roy_T.AStar.Graphs;
+using Roy_T.AStar.Grids;
 using Roy_T.AStar.Paths;
 using Roy_T.AStar.Primitives;
 
@@ -12,6 +13,43 @@ namespace Roy_T.AStar.Tests
         public PathFinderTests()
         {
             this.PathFinder = new PathFinder();
+        }
+
+
+        [Test]
+        public void Issue50()
+        {
+            var columns = 21;
+            var rows = 21;
+            var start = new GridPosition(0, 20);
+            var end = new GridPosition(16, 2);
+
+            var grid = Grid.CreateGridWithLateralAndDiagonalConnections(new GridSize(columns, rows), new Size(Distance.FromMeters(1), Distance.FromMeters(1)), Velocity.FromMetersPerSecond(1.0f));
+
+            grid.DisconnectNode(new GridPosition(7, 3));
+            grid.DisconnectNode(new GridPosition(8, 3));
+            grid.DisconnectNode(new GridPosition(9, 3));
+            grid.DisconnectNode(new GridPosition(10, 3));
+            grid.DisconnectNode(new GridPosition(11, 3));
+            grid.DisconnectNode(new GridPosition(12, 3));
+            grid.DisconnectNode(new GridPosition(13, 3));
+            grid.DisconnectNode(new GridPosition(14, 3));
+
+            grid.DisconnectNode(new GridPosition(14, 4));
+            grid.DisconnectNode(new GridPosition(14, 5));
+            grid.DisconnectNode(new GridPosition(14, 6));
+            grid.DisconnectNode(new GridPosition(14, 7));
+            grid.DisconnectNode(new GridPosition(14, 8));
+            grid.DisconnectNode(new GridPosition(14, 9));
+            grid.DisconnectNode(new GridPosition(14, 10));
+            grid.DisconnectNode(new GridPosition(14, 11));
+            grid.DisconnectNode(new GridPosition(14, 12));
+
+            var pathA = this.PathFinder.FindPath(start, end, grid);
+            Assert.That(pathA.Edges.Count < 32);
+
+            var pathB = this.PathFinder.FindPath(end, start, grid);
+            Assert.That(pathB.Edges.Count < 32);
         }
 
         [Test]
